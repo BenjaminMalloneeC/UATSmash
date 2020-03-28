fe04700a108327e1b16733f252d8a25d07480496
BenjaminMalloneeC <ben.mallonee.c@gmail>
2020-03-27 20:27:29 -0600

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [HideInInspector] public int currentScene = 0;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Warning: A second game manager wasd detected and destroyed.");
+        }
+    }
+
+    /// <summary>
+    /// Load the scene passed in as a parameter.
+    /// </summary>
+    /// <param name="levelToLoad">Name of the level to load.</param>
+    public void LoadLevel(string levelToLoad)
+    {
+        SceneManager.LoadScene(levelToLoad);
+        //Potentially dangerous
+        currentScene++;
+    }
+
+    public void LoadLevel(int indexToLoad)
+    {
+        SceneManager.LoadScene(indexToLoad);
+        currentScene = indexToLoad;
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentScene + 1);
+    }
+}
